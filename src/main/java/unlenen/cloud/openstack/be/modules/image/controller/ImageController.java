e90e7010bd6fe97408eabebb4b29ba5db2a7c390
Nebi Volkan ÜNLENEN <unlenen@gmail.com>
2022-06-28 08:51:05 +0300

    Image Controllers

--- src/main/java/unlenen/cloud/openstack/be/modules/image/controller/ImageController.java
+++ src/main/java/unlenen/cloud/openstack/be/modules/image/controller/ImageController.java
@@ -2,12 +2,21 @@ package unlenen.cloud.openstack.be.modules.image.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import unlenen.cloud.openstack.be.exception.UnvalidCallException;
 import unlenen.cloud.openstack.be.model.response.ErrorInfo;
 import unlenen.cloud.openstack.be.model.response.OpenStackResponse;
-import unlenen.cloud.openstack.be.modules.identity.service.IdentityService;
+import unlenen.cloud.openstack.be.modules.image.models.ImageContainerFormat;
+import unlenen.cloud.openstack.be.modules.image.models.ImageDiskFormat;
+import unlenen.cloud.openstack.be.modules.image.service.ImageService;
 
 /**
  *
@@ -18,7 +27,58 @@ import unlenen.cloud.openstack.be.modules.identity.service.IdentityService;
 public class ImageController {
 
     @Autowired
-    IdentityService identityService;
+    ImageService imageService;
+
+    @GetMapping("/images")
+    public ResponseEntity<OpenStackResponse> getImages(
+            @RequestHeader("token") String token,
+            @RequestParam(required = false, name = "name") String name,
+            @RequestParam(required = false, name = "tags") String tags
+    ) {
+        OpenStackResponse openStackResponse = new OpenStackResponse();
+        HttpStatus httpStatus;
+        try {
+            openStackResponse.setOpenStackResult(imageService.getImages(token, name, tags));
+            httpStatus = HttpStatus.OK;
+        } catch (Exception e) {
+            httpStatus = handleError(openStackResponse, e);
+        }
+        return new ResponseEntity<OpenStackResponse>(openStackResponse, httpStatus);
+    }
+
+    @PostMapping("/image/{name}")
+    public ResponseEntity<OpenStackResponse> createImage(
+            @RequestHeader("token") String token,
+            @PathVariable() String name,
+            @RequestParam(required = true, name = "diskFormat") ImageDiskFormat diskFormat,
+            @RequestParam(required = true, name = "containerFormat") ImageContainerFormat containerFormat
+    ) {
+        OpenStackResponse openStackResponse = new OpenStackResponse();
+        HttpStatus httpStatus;
+        try {
+            openStackResponse.setOpenStackResult(imageService.createImage(token, name, diskFormat, containerFormat));
+            httpStatus = HttpStatus.CREATED;
+        } catch (Exception e) {
+            httpStatus = handleError(openStackResponse, e);
+        }
+        return new ResponseEntity<OpenStackResponse>(openStackResponse, httpStatus);
+    }
+
+    @DeleteMapping("/image/{imageId}")
+    public ResponseEntity<OpenStackResponse> deleteImage(
+            @RequestHeader("token") String token,
+            @PathVariable() String imageId
+    ) {
+        OpenStackResponse openStackResponse = new OpenStackResponse();
+        HttpStatus httpStatus;
+        try {
+            imageService.deleteImage(token, imageId);
+            httpStatus = HttpStatus.NO_CONTENT;
+        } catch (Exception e) {
+            httpStatus = handleError(openStackResponse, e);
+        }
+        return new ResponseEntity<OpenStackResponse>(openStackResponse, httpStatus);
+    }
 
     private HttpStatus handleError(OpenStackResponse openStackResponse, Exception e) {
         HttpStatus httpStatus;
